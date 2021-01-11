02b50239840e02757b905cb42d2ef2f1d339a5f1
Christian Krivickis <S00198911@mail.itsligo.ie>
2021-01-11 10:30:00 +0000

    Added account class

---
+++ Exam/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public abstract class Account
+    {
+        // Constructor - First Name
+        public string FirstName { get; set; }
+
+        // Constructor - Last Name
+        public string LastName { get; set; }
+
+        // Constructor - Balance
+        public decimal Balance { get; set; }
+
+        // Constructor - Balance
+        public decimal Interest { get; set; }
+
+        // Account object
+        public Account(string FirstName, string LastName, decimal Balance, decimal Interest)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Balance = Balance;
+            this.Interest = Interest;
+        }
+
+        public abstract decimal CalculateInterest();
+    }
+
+    public class CurrentAccount : Account
+    {
+        public CurrentAccount(string FirstName, string LastName, decimal Balance, decimal Interest) : base(FirstName, LastName, Balance, Interest)
+        {
+        }
+
+        public override decimal CalculateInterest()
+        {
+            return Balance * (decimal)0.03;
+        }
+    }
+
+    public class SavingsAccount : Account
+    {
+        public SavingsAccount(string FirstName, string LastName, decimal Balance, decimal Interest) : base(FirstName, LastName, Balance, Interest)
+        {
+        }
+
+        public override decimal CalculateInterest()
+        {
+            return Balance * (decimal)0.06;
+        }
+    }
+}
--- Exam/MainWindow.xaml.cs
+++ Exam/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
 
 namespace Exam
 {
@@ -20,9 +22,52 @@ namespace Exam
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Account> Accounts = new ObservableCollection<Account>();
+        ObservableCollection<Account> filteredAccounts = new ObservableCollection<Account>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Part time employees (First name, last name, status, hourly rate, hours worked)
+            CurrentAccount ca1 = new CurrentAccount("Jane", "Jones", "Part Time", 10, 15);
+            CurrentAccount ca2 = new CurrentAccount("John", "Smith", "Part Time", 15, 14);
+
+            // Full time employees (First name, last name, status, salary)
+            SavingsAccount sat1 = new SavingsAccount("Joe", "Murphy", "Full Time", 24000);
+            SavingsAccount sa2 = new SavingsAccount("Jess", "Walsh", "Full Time", 25000);
+
+            // Added Full time and Part time employees to observable list
+            Accounts.Add(pt1);
+            Accounts.Add(pt2);
+            Accounts.Add(ft1);
+            Accounts.Add(ft2);
+        }
+
+        private void buttonDeposit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonWithdraw_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonInterest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBoxTransactionAmount_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Clears text in textbox when clicked on
+            textBoxTransactionAmount.Text = "";
+        }
+
+  
     }
 }
