c94e8b7cdde71280923c8f4603cb2b91d552bdd3
Christian Krivickis <S00198911@mail.itsligo.ie>
2021-01-11 11:28:00 +0000

    Finished

--- Exam/Account.cs
+++ Exam/Account.cs
@@ -17,6 +17,9 @@ namespace Exam
         // Constructor - Last Name
         public string LastName { get; set; }
 
+        // Constructor - Account Type
+        public string Type { get; set; }
+
         // Constructor - Balance
         public decimal Balance { get; set; }
 
@@ -24,40 +27,47 @@ namespace Exam
         public decimal Interest { get; set; }
 
         // Account object
-        public Account(string FirstName, string LastName, int AccountNumber, decimal Balance)
+        public Account(string FirstName, string LastName, int AccountNumber, decimal Balance, string Type)
         {
             this.FirstName = FirstName;
             this.LastName = LastName;
             this.AccountNumber = AccountNumber;
             this.Balance = Balance;
+            this.Type = Type;
         }
 
+        // Abstract method to calculate interest
         public abstract decimal CalculateInterest();
 
+        // ToString method to properly format accounts
         public override string ToString()
         {
             return string.Format($"{AccountNumber} - {LastName}, {FirstName}");
         }
     }
 
+    // Current Account class - Inherited Account class
     public class CurrentAccount : Account
     {
-        public CurrentAccount(string FirstName, string LastName, int AccountNumber, decimal Balance) : base(FirstName, LastName, AccountNumber, Balance)
+        public CurrentAccount(string FirstName, string LastName, int AccountNumber, decimal Balance, string Type) : base(FirstName, LastName, AccountNumber, Balance, Type)
         {
         }
 
+        // Method to interest of Current Accounts (Balance divided by 3%)
         public override decimal CalculateInterest()
         {
             return Balance * (decimal)0.03;
         }
     }
 
+    // Savings Account class - Inherited Account class
     public class SavingsAccount : Account
     {
-        public SavingsAccount(string FirstName, string LastName, int AccountNumber, decimal Balance) : base(FirstName, LastName, AccountNumber, Balance)
+        public SavingsAccount(string FirstName, string LastName, int AccountNumber, decimal Balance, string Type) : base(FirstName, LastName, AccountNumber, Balance, Type)
         {
         }
 
+        // Method to interest of Savings Accounts (Balance divided by 6%)
         public override decimal CalculateInterest()
         {
             return Balance * (decimal)0.06;
--- Exam/MainWindow.xaml.cs
+++ Exam/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace Exam
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Observable List
         ObservableCollection<Account> Accounts = new ObservableCollection<Account>();
         ObservableCollection<Account> filteredAccounts = new ObservableCollection<Account>();
 
@@ -30,17 +31,18 @@ namespace Exam
             InitializeComponent();
         }
 
+        // Method runs when window is loaded
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // 
-            CurrentAccount ca1 = new CurrentAccount("Jane", "Jones", 45748275, 1500);
-            CurrentAccount ca2 = new CurrentAccount("John", "Smith", 679569, 30000);
+            // Current Accounts (First Name, Last Name, Account Number, Balance, Account Type)
+            CurrentAccount ca1 = new CurrentAccount("Jane", "Jones", 45748275, 1500, "Current Account");
+            CurrentAccount ca2 = new CurrentAccount("John", "Smith", 679569, 30000, "Current Account");
 
-            // 
-            SavingsAccount sa1 = new SavingsAccount("Joe", "Murphy", 95684, 4000);
-            SavingsAccount sa2 = new SavingsAccount("Jess", "Walsh", 7535737, 9999);
+            // Savings Accounts (First Name, Last Name, Account Number, Balance, Account Type)
+            SavingsAccount sa1 = new SavingsAccount("Joe", "Murphy", 95684, 4000, "Savings Account");
+            SavingsAccount sa2 = new SavingsAccount("Jess", "Walsh", 7535737, 9999, "Savings Account");
 
-            // 
+            // Adds 4 objects to ObservableCollection<Account> Accounts
             Accounts.Add(ca1);
             Accounts.Add(ca2);
             Accounts.Add(sa1);
@@ -50,16 +52,19 @@ namespace Exam
 
         }
 
+        // Method runs when buttonDeposit is clicked
         private void buttonDeposit_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        // Method runs when buttonWithdraw_Click is clicked
         private void buttonWithdraw_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        // Method runs when buttonInterest_Click is clicked
         private void buttonInterest_Click(object sender, RoutedEventArgs e)
         {
 
@@ -71,6 +76,7 @@ namespace Exam
             textBoxTransactionAmount.Text = "";
         }
 
+        // Code for selecting accounts from list
         private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             CurrentAccount selectedCA = listBox.SelectedItem as CurrentAccount;
@@ -83,6 +89,7 @@ namespace Exam
                 textBlockFirstName.Text = selectedCA.FirstName;
                 textBlockLastName.Text = selectedCA.LastName;
                 textBlockBalance.Text = selectedCA.Balance.ToString();
+                textBlockAccountType.Text = selectedCA.Type;
             }
 
             // If Savings Account is pressed - this if statement runs and data is displayed
@@ -91,6 +98,50 @@ namespace Exam
                 textBlockFirstName.Text = selectedSA.FirstName;
                 textBlockLastName.Text = selectedSA.LastName;
                 textBlockBalance.Text = selectedSA.Balance.ToString();
+                textBlockAccountType.Text = selectedSA.Type;
+            }
+        }
+
+        // Method runs when either/ neither/ both checkboxCurrent or checkboxSavings are pressed 
+        private void checkBox_Click(object sender, RoutedEventArgs e)
+        {
+            // Run Filter() Method
+            Filter();
+        }
+
+        // Method to filter which accounts are observable on the list
+        // DOES NOT WORK
+        private void Filter()
+        {
+            // Removes filtered acccounts from the list
+            filteredAccounts.Clear();
+            listBox.ItemsSource = null;
+
+
+            // If checkboxCurrent equals true (checked) - this if statement runs
+            if (checkBoxCurrent.IsChecked == true)
+            {
+                foreach (Account Accounts in Accounts)
+                {
+                    if (Accounts is CurrentAccount)
+                    {
+                        // If the account is a current account , they will be added to the filtered account
+                        filteredAccounts.Add(Accounts);
+                    }
+                }
+            }
+
+            // If checkboxSavings equals true (checked) - this if statement runs
+            if (checkBoxSavings.IsChecked == true)
+            {
+                foreach (Account Accounts in Accounts)
+                {
+                    if (Accounts is SavingsAccount)
+                    {
+                        // If the account is a savings account, they will be added to the filtered account
+                        filteredAccounts.Add(Accounts);
+                    }
+                }
             }
         }
     }
